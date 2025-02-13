81beddd085b0d12e4345dfd99a5b3592baa73132
antonio-wachave <anthonypaulino32@gmail.com>
2025-02-13 14:12:52 +0200

    update filter and authatication

--- src/main/java/com/wachave/todolist/user/controller/UserModelController.java
+++ src/main/java/com/wachave/todolist/user/controller/UserModelController.java
@@ -1,5 +1,6 @@
 package com.wachave.todolist.user.controller;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.wachave.todolist.user.entity.UserModel;
 import com.wachave.todolist.user.repository.UserModelRepository;
 import com.wachave.todolist.user.service.UserModelService;
@@ -27,11 +28,13 @@ public class UserModelController {
         UserModel user = this.userModelService.findByUsername(userModel.getUsername());
 
         if(user != null){
-
             System.out.print(" usuario ja existe! ");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existe!") ;
-
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12,userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         UserModel createdUser = this.userModelRepository.save(userModel);
 
